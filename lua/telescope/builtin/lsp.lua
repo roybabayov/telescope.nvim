28e490e8eee8ad4afb25ee29c4d7e4aa4eddec22
roybabayov <roybabayov@gmail.com>
2021-12-17 04:00:46 +0200

    tmp

--- lua/telescope/builtin/lsp.lua
+++ lua/telescope/builtin/lsp.lua
@@ -50,7 +50,7 @@ lsp.references = function(opts)
                 print("err", file_uri, buffer_nr)
                 for key2, entry2 in pairs(locations) do
                     if entry2.filename == entry.filename then
-                        locations[key2].entry_type = "e---"
+                        --locations[key2].entry_type = "e---"
                         print("err", file_uri, buffer_nr, locations[key2].lnum - 1, locations[key2].col - 1)
                     end
                 end
@@ -73,8 +73,9 @@ lsp.references = function(opts)
                                     end
                                 end
                             end
-                            locations[key2].entry_type = string.format("-%s%s%s", res_text, res_read, res_write)
-                            print("err", file_uri, buffer_nr, locations[key2].lnum - 1, locations[key2].col - 1, locations[key2].entry_type)
+                            entry_type = string.format("-%s%s%s", res_text, res_read, res_write)
+                            --locations[key2].entry_type = entry_type
+                            print(file_uri, buffer_nr, locations[key2].lnum - 1, locations[key2].col - 1, entry_type)
                         end
                     end 
                 end
